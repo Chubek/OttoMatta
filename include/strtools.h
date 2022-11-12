36bff8f5c76753b593ff2311e5cf78a746061a40
Chubak Bidpaa <chubakbidpaa@gmail.com>
2022-11-12 16:19:10 +0330

    comments added

--- include/strtools.h
+++ include/strtools.h
@@ -6,14 +6,14 @@ pattern.
 typedef struct {
     char *data;
     size_t start, end;
-    int *ref_counter;
+    int *ref_counter; //pointer to the integer holding number of copies
 } strt_s;
 
 strt_s *strt_new(char const *in);
 strt_s *strt_copy(strt_s const *in, size_t start, size_t len);
 void strt_free(strt_s *in);
 
-
+//the list containing separated substrs
 typedef struct {
     strt_s **strings;
     int count;
